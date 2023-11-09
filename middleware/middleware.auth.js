10b45d1f86c94d3d1fd425f8066cbd4c54e9f007
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-11-09 22:06:39 +0530

    project

--- middleware/middleware.auth.js
+++ middleware/middleware.auth.js
@@ -1,21 +1,21 @@
-const jwt = require("jsonwebtoken")
+// const jwt = require("jsonwebtoken")
 
-const authenticate = (req, res, next) => {
-    const token = req.headers.authorization
-    console.log(token)
-    if (token) {
-        const decoded = jwt.verify(token, "shhhhh")
-        if (decoded) {
-            req.body.userId=decoded.userId
-            // console.log(decoded)
-            next()
-        } else {
-            res.status(400).send({"msg":"Please Login first"})
-        }
-    } else {
-        res.status(400).send({"msg":"Please Login first"})
-    }
-}
-module.exports = {
-    authenticate
-}
+// const authenticate = (req, res, next) => {
+//     const token = req.headers.authorization
+//     console.log(token)
+//     if (token) {
+//         const decoded = jwt.verify(token, "shhhhh")
+//         if (decoded) {
+//             req.body.userId=decoded.userId
+//             // console.log(decoded)
+//             next()
+//         } else {
+//             res.status(400).send({"msg":"Please Login first"})
+//         }
+//     } else {
+//         res.status(400).send({"msg":"Please Login first"})
+//     }
+// }
+// module.exports = {
+//     authenticate
+// }
